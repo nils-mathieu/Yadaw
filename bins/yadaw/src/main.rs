61c02d2773bcfdcbc6422fc8d8f16e43f3ec848f
Nils <nils.mathieu.contact@gmail.com>
2025-01-21 04:54:38 +0100

    remove testing code

--- bins/yadaw/src/main.rs
+++ bins/yadaw/src/main.rs
@@ -1,30 +1,12 @@
-use {
-    kui::winit::{dpi::PhysicalSize, window::WindowAttributes},
-    std::time::Duration,
-};
+use kui::winit::{dpi::PhysicalSize, window::WindowAttributes};
 
 /// The glorious entry point of the Yadaw application.
 fn main() {
     kui::run(|ctx| {
-        let window = ctx.create_window(
+        ctx.create_window(
             WindowAttributes::default()
                 .with_title("Yadaw")
                 .with_inner_size(PhysicalSize::new(1280, 720)),
         );
-
-        let ctx2 = ctx.clone();
-        ctx.call_after(Duration::from_secs(3), move || {
-            window.close();
-            ctx2.create_window(
-                WindowAttributes::default()
-                    .with_title("Yadaw 2")
-                    .with_inner_size(PhysicalSize::new(1280, 720)),
-            );
-
-            let ctx3 = ctx2.clone();
-            ctx2.call_after(Duration::from_secs(3), move || {
-                ctx3.exit();
-            });
-        });
     });
 }
