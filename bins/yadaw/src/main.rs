c8f80857a312912c4e961556b3d26043a9dd0362
Nils <nils.mathieu.contact@gmail.com>
2025-01-23 04:09:39 +0100

    remove dummy audio code

--- bins/yadaw/src/main.rs
+++ bins/yadaw/src/main.rs
@@ -1,20 +1,12 @@
-use {
-    kui::{
-        elem,
-        elements::text::TextResource,
-        peniko::Color,
-        winit::{dpi::PhysicalSize, window::WindowAttributes},
-    },
-    std::sync::atomic::{AtomicBool, Ordering},
+use kui::{
+    elem,
+    elements::text::TextResource,
+    peniko::Color,
+    winit::{dpi::PhysicalSize, window::WindowAttributes},
 };
-
-static PAUSED: AtomicBool = AtomicBool::new(true);
 
 /// The glorious entry point of the Yadaw application.
 fn main() {
-    initialize_audio_thread()
-        .unwrap_or_else(|err| panic!("Failed to initialize audio thread: {err}"));
-
     kui::run(|ctx| {
         initialize_fonts(&ctx).unwrap_or_else(|err| panic!("Failed to register fonts: {err}"));
 
@@ -30,7 +22,6 @@ fn main() {
 
                 kui::elements::button {
                     child: make_button();
-                    on_click: move || {PAUSED.fetch_xor(true, Ordering::Relaxed);};
                 }
             }
         });
@@ -95,73 +86,4 @@ fn initialize_fonts(ctx: &kui::Ctx) -> std::io::Result<()> {
         }
         Ok(())
     })
-}
-
-/// Initializes the audio thread.
-fn initialize_audio_thread() -> Result<(), adevice::Error> {
-    let host = match adevice::default_host()? {
-        Some(host) => host,
-
-        // No host is available on the current platform. There is nothing we can do about it.
-        None => return Ok(()),
-    };
-
-    let device = match host.default_output_device(adevice::RoleHint::Games)? {
-        Some(device) => device,
-
-        // No output device is available. There is nothing we can do about it.
-        None => return Ok(()),
-    };
-
-    let stream_config = device
-        .output_formats(adevice::ShareMode::Share)?
-        .unwrap()
-        .to_stream_config(
-            adevice::ShareMode::Share,
-            2,
-            &[],
-            adevice::ChannelLayout::Separate,
-            512,
-            44100.0,
-        );
-
-    assert!(stream_config.format == adevice::Format::F32Le);
-    assert!(stream_config.channel_count == 2);
-    assert!(stream_config.channel_encoding == adevice::ChannelLayout::Interleaved);
-
-    let mut phase: f64 = 0.0;
-    let mut amp: f64 = 0.0;
-
-    let stream = device.open_output_stream(
-        stream_config,
-        Box::new(move |mut callback| {
-            let data = unsafe { callback.get_interleaved_output_buffer::<f32>(2) };
-
-            let target_amp = if PAUSED.load(Ordering::Relaxed) {
-                0.0
-            } else {
-                0.8
-            };
-
-            let mut i = 0;
-            while i < data.len() {
-                if i % 2 == 0 {
-                    amp += (target_amp - amp) * 0.005;
-                }
-
-                let val = amp * (std::f64::consts::TAU * phase).sin();
-                data[i] = val as f32;
-                data[i + 1] = val as f32;
-
-                phase += 440.0 / 44100.0;
-                if phase >= 1.0 {
-                    phase -= 1.0;
-                }
-                i += 2;
-            }
-        }),
-    )?;
-    stream.start()?;
-    Box::leak(stream);
-    Ok(())
 }
